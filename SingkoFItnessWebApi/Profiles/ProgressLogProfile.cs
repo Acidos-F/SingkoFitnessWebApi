0e22a7a58de57683aad4aebde8a0015ac0b8a4e6
Kerby Cruz <kerbyllamosocruz@gmail.com>
2025-09-23 15:47:34 +0800

    Add files for ProgressLog feature

---
+++ SingkoFItnessWebApi/Profiles/ProgressLogProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SingkoFItnessWebApi.Dtos;
+using SingkoFItnessWebApi.Models;
+
+namespace SingkoFItnessWebApi.Profiles {
+    public class ProgressLogProfile : Profile {
+        public ProgressLogProfile() {
+            CreateMap<ProgressLog, ProgressLogsReadDto>().ReverseMap();
+            CreateMap<ProgressLogsCreateDto, ProgressLog>();
+            CreateMap<ProgressLogsUpdateDto, ProgressLog>();
+        }
+    }
+}
