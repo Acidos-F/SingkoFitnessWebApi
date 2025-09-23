fef0864d49d5182ccb6fff744b513bd693390c36
Kerby Cruz <kerbyllamosocruz@gmail.com>
2025-09-23 15:47:34 +0800

    Add files for ProgressLog feature

---
+++ SingkoFItnessWebApi/Dtos/ProgressLogDto/ProgressLogsReadDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace SingkoFItnessWebApi.Dtos {
+    public class ProgressLogsReadDto {
+        public int ProgressId { get; set; }
+        public int UserId { get; set; }
+        public DateOnly Date { get; set; }
+        public decimal? Weight { get; set; }
+        public decimal? BodyFatPercentage { get; set; }
+        public decimal? MuscleMass { get; set; }
+    }
+}
