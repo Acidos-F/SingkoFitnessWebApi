ff82646cd7a70d2fd5e71c4337dfb4f7dd3edf8f
Makchoppers <pabayamark@gmail.com>
2025-09-25 07:24:49 +0800

    eto na ata

---
+++ SingkoFItnessWebApi/Dtos/NutrionalLogDto/NutritionLogUpdateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SingkoFItnessWebApi.Dtos.NutrionalLogDto
+{
+    public class NutritionLogUpdateDto
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Meal type cannot exceed 50 characters.")]
+        public string MealType { get; set; }
+
+        [Required]
+        [Range(0, 10000, ErrorMessage = "Calories must be between 0 and 10,000.")]
+        public int Calories { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Protein must be between 0 and 1000 grams.")]
+        public decimal? Protein { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Carbs must be between 0 and 1000 grams.")]
+        public decimal? Carbs { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "Fat must be between 0 and 1000 grams.")]
+        public decimal? Fat { get; set; }
+
+    }
+}
---
+++ SingkoFItnessWebApi/Profiles/NutritionalLogProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SingkoFItnessWebApi.Dtos;
+using SingkoFItnessWebApi.Dtos.NutrionalLogDto;
+using SingkoFItnessWebApi.Models;
+
+namespace SingkoFItnessWebApi.Profiles
+{
+    public class NutritionalLogProfile:Profile{
+        public NutritionalLogProfile()
+        {
+            CreateMap<NutritionLog, NutritionLogReadDto>().ReverseMap();
+            CreateMap<NutritionLogCreateDto, NutritionLog>();
+            CreateMap<NutritionLogUpdateDto, NutritionLog>();
+        }
+    }
+
+}
