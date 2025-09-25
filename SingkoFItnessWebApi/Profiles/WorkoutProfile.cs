c5b761a482371969bbeb26c8c7a3afb5787c8ab5
Kerby Cruz <kerbyllamosocruz@gmail.com>
2025-09-25 10:28:49 +0800

    Added Workout Controller, Dtos, and Profile

---
+++ SingkoFItnessWebApi/Profiles/WorkoutProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SingkoFitnessWebApi.Dtos.Exercise;
+using SingkoFItnessWebApi.Dtos;
+using SingkoFItnessWebApi.Models;
+
+namespace SingkoFitnessWebApi.Profiles {
+    public class WorkoutProfile : Profile {
+        public WorkoutProfile() {
+            // Workout → WorkoutReadDto
+            CreateMap<Workout, WorkoutReadDto>();
+
+            // WorkoutCreateDto → Workout
+            CreateMap<WorkoutCreateDto, Workout>();
+
+            // WorkoutUpdateDto → Workout
+            CreateMap<WorkoutUpdateDto, Workout>();
+
+            // Exercise → ExerciseReadDto
+            CreateMap<Exercise, ExerciseReadDto>();
+        }
+    }
+}
